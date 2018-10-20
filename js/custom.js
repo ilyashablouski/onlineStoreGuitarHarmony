f8ea0c9695a166cdcd6efd06530f629673244407
Ilya Shablovskiy <nil199308@gmail.com>
2018-10-20 13:05:59 +0300

    ""

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,28 @@
+// Слайдер
 $( document ).ready(function() {
   $('.slider').glide({
     autoplay: 3500,
-    hoverpause: true, // set to false for nonstop rotate
+    hoverpause: true,
     arrowRightText: '&rarr;',
     arrowLeftText: '&larr;'
   })
+});
+
+// Кнопка сколлинга
+$(document).ready(function(){
+	$(window).scroll(function(){ 
+		if($(window).scrollTop()>=300){
+			$('.gul-up').show('fast');
+		}else{
+			$('.gul-up').hide('fast');
+		}
+    });
+    if($(window).scrollTop()>=300){
+		$('.gul-up').css('display', 'block');
+	}else{
+		$('.gul-up').css('display', 'none');
+	}
+    $('.gul-up').click(function () { 
+		$('html, body').animate({scrollTop:0}, 500);            
+    });
 });
