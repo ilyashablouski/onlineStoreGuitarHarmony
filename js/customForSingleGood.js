12dfc66f9b1b289a4ca514f1361377a663804cc1
Ilya Shablovskiy <nil199308@gmail.com>
2018-10-25 19:33:47 +0300

    ""

--- js/customForSingleGood.js
+++ js/customForSingleGood.js
@@ -1,16 +1,14 @@
-//Switcher
+//Switcher+zoom
 $(document).ready(function () {
     $('.reduced-image img').click(function() {
 		var attr = $(this).attr('src');
-		$('.main-image img').attr('src', attr);
+    $('.main-image img').attr('src', attr);
+    $('.zoom-effect').trigger('zoom.destroy');
+    $('.zoom-effect').zoom();
     });
 });
 
 //jQuery Zoom
 $(document).ready(function () {
-    $('.zoom-effect')
-    .wrap('<div style="display:inline-block"></div>')
-    .css('display', 'block')
-    .parent()
-    .zoom();
+  $('.zoom-effect').zoom();
 });
