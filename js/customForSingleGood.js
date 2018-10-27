c60aefd6093b8195c4011e561599dac8d05e0f41
Ilya Shablovskiy <nil199308@gmail.com>
2018-10-28 02:31:04 +0300

    last edit static page

--- js/customForSingleGood.js
+++ js/customForSingleGood.js
@@ -1,3 +1,24 @@
+// Scrolling button
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(window).scrollTop() >= 300) {
+			$('.gul-up').show('fast');
+		} else {
+			$('.gul-up').hide('fast');
+		}
+	});
+	if ($(window).scrollTop() >= 300) {
+		$('.gul-up').css('display', 'block');
+	} else {
+		$('.gul-up').css('display', 'none');
+	}
+	$('.gul-up').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+	});
+});
+
 //Switcher+zoom
 $(document).ready(function () {
     $('.reduced-image img').click(function() {
